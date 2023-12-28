182746727d946fbc71a2092b2ef38c06bf62d1ad
jaye <jaye@greydientlab.com>
2023-12-28 18:58:57 +0800

    updated buttons

--- frontend/components/atoms/buttons/button.php
+++ frontend/components/atoms/buttons/button.php
@@ -11,64 +11,88 @@
  * @package greydientlab
  */
 
-$class         = $args['class'] ?? '';
-$button_style  = $args['button_style'] ?? 'default';
-$button_height = $args['button_height'] ?? 'default';
-$link_url      = $args['url'] ?? false;
-$link_title    = $args['title'] ?? '';
-$link_target   = $args['target'] ?? '_self';
-$icon          = $args['icon'] ?? '';
+$gl_btn_size      = $args['gl_btn_size'] ?? 'btn-sm';
+$gl_btn_type      = $args['gl_btn_type'] ?? 'btn-primary';
+$gl_btn_text      = $args['gl_btn_text'] ?? 'Button text';
+$gl_is_rounded    = $args['gl_is_rounded'] ?? false;
+$gl_leading_icon  = $args['gl_leading_icon'] ?? false;
+$gl_trailing_icon = $args['gl_trailing_icon'] ?? false;
 
-$icon      = 'arrow' === $button_style ? 'arrow-right' : $icon;
-$file_name = $icon ? get_stylesheet_directory() . '/frontend/static/icons/' . $icon . '.svg' : '';
+$btn_class = '';
 
-$link_base   = ' inline-flex items-center justify-center leading-1.125 inline-block';
-$button_base = ' text-center transition-settings-default text-white cursor-pointer';
-$icon_class  = 'link' === $button_style || 'arrow' === $button_style ? 'ml-.5 mt-1px w-1 h-1 transition-settings-default hover-target' : 'mr-1 opacity-50';
+switch ( $gl_btn_size ) {
+	case 'btn-xs':
+		$btn_class .= $gl_is_rounded ? 'rounded-full' : 'rounded';
+		$btn_class .= $gl_is_rounded ? ' px-2.5 py-1' : ' px-2 py-1';
+		$btn_class .= ' text-xs font-semibold shadow-sm';
+		break;
+
+	case 'btn-sm':
+		$btn_class .= $gl_is_rounded ? 'rounded-full' : 'rounded';
+		$btn_class .= $gl_is_rounded ? ' px-2.5 py-1' : ' px-2 py-1';
+		$btn_class .= ' text-sm font-semibold shadow-sm';
+		break;
+
+	case 'btn-md':
+		$btn_class .= $gl_is_rounded ? 'rounded-full' : 'rounded-md';
+		$btn_class .= $gl_is_rounded ? ' px-3 py-1.5' : ' px-2.5 py-1.5';
+		$btn_class .= ' text-sm font-semibold shadow-sm';
+		break;
+
+	case 'btn-lg':
+		$btn_class .= $gl_is_rounded ? 'rounded-full' : 'rounded-md';
+		$btn_class .= $gl_is_rounded ? ' px-3.5 py-2' : ' px-3 py-2';
+		$btn_class .= ' text-sm font-semibold shadow-sm';
+		break;
+
+	case 'btn-xl':
+		$btn_class .= $gl_is_rounded ? 'rounded-full' : 'rounded-md';
+		$btn_class .= $gl_is_rounded ? ' px-4 py-2.5' : ' px-3.5 py-2.5';
+		$btn_class .= ' px-3.5 py-2.5 text-sm';
+		break;
+
+	default:
+		break;
+}
+
+switch ( $gl_btn_type ) {
+	case 'btn-primary':
+		$btn_class .= ' bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600';
+		break;
+
+	case 'btn-secondary':
+		$btn_class .= ' bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50';
+		break;
+
+	case 'btn-primary-dark':
+		$btn_class .= ' bg-indigo-500 text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500';
+		break;
+
+	case 'btn-secondary-dark':
+		$btn_class .= ' bg-white/10 text-white hover:bg-white/20';
+		break;
+
+	case 'btn-soft':
+		$btn_class .= ' bg-indigo-50 text-indigo-600 hover:bg-indigo-100';
+		break;
+
+	default:
+		break;
+}
+
+if ( $gl_leading_icon || $gl_trailing_icon ) {
+	$btn_class .= ' inline-flex items-center gap-x-1.5';
+}
 
 ?>
+<button type="button" class="font-semibold shadow-sm mt-0 <?php echo esc_attr( $btn_class ); ?>">
+	<?php if ( $gl_leading_icon ) : ?>
+		<img width="20" height="20" src="<?php echo esc_url( $gl_leading_icon ); ?>" alt="icon">
+	<?php endif; ?>
 
-<?php if ( $link_url ) : ?>
+	<?php echo esc_html( $gl_btn_text ); ?>
 
-	<a
-		class="<?php echo esc_attr( $class ); ?>"
-		href="<?php echo esc_url( $link_url ); ?>"
-		target="<?php echo esc_attr( $link_target ); ?>"
-	>
-
-<?php else : ?>
-
-	<button
-		class="<?php echo esc_attr( $class ); ?>"
-		type="button"
-	>
-
-<?php endif; ?>
-
-		<?php if ( $icon ) : ?>
-
-			<span class="<?php echo esc_attr( $icon_class ); ?>">
-
-				<?php
-				// @codingStandardsIgnoreLine
-				$file_content = file_get_contents( $file_name ) ?? '';
-				// @codingStandardsIgnoreLine - we are using a fixed set of known SVGs so there is no risk here.
-				echo $file_content;
-				?>
-
-		</span>
-
-		<?php endif; ?>
-
-		<?php echo esc_html( $link_title ); ?>
-
-
-<?php if ( $link_url ) : ?>
-
-	</a>
-
-<?php else : ?>
-
-	</button>
-
-<?php endif; ?>
+	<?php if ( $gl_trailing_icon ) : ?>
+		<img width="20" height="20" src="<?php echo esc_url( $gl_trailing_icon ); ?>" alt="icon">
+	<?php endif; ?>
+</button>
