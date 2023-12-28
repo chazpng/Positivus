0c22d253ca6ad78bb4edf7df080eac03a4e16bfb
jaye <jaye@greydientlab.com>
2023-12-28 18:58:42 +0800

    added optional classes

--- acf/blocks/php-code/template.php
+++ acf/blocks/php-code/template.php
@@ -5,9 +5,10 @@
  * @package circles_x
  */
 
+$class_name = $block['className'] ?? 'min-h-64';
 ?>
 
-<div class="gl-b-php-code min-h-64">
+<div class="gl-b-php-code <?php echo esc_attr( $class_name ); ?>">
 	<?php
 	$php_code = get_field( 'php_code' );
 
