fb573ce0fb45efa5e251f6ef1c9a8bcfe10c5401
jaye <jaye@greydientlab.com>
2024-01-13 17:43:45 +0800

    fixed lint errors

--- acf/blocks/cta-sections/fields.php
+++ acf/blocks/cta-sections/fields.php
@@ -10,62 +10,62 @@
  */
 
 $cta = new StoutLogic\AcfBuilder\FieldsBuilder( 'cta' );
-$cta
-
-->addSelect(
+$cta->addSelect(
 	'cta_style',
 	array(
 		'default_value' => 'simple',
 	)
 )
 
-
-
-->addChoices(
-	array( 'simple' => 'Simple' ),
-	array( 'two-columns' => 'Two Columns with Photo on dark' ),
-	array( 'with-panel' => 'With Panel on dark' ),
-	array( 'split-with-image' => 'Split with Image' ),
-)
-->addCheckbox(
-	'checkbox_field',
-	array(
-		'label'         => 'On Brand',
-		'choices'       => array( 'is Background on brand?' ),
-		'default_value' => array( 0 ),
-		'return_format' => 'value',
+	->addChoices(
+		array( 'simple' => 'Simple' ),
+		array( 'two-columns' => 'Two Columns with Photo on dark' ),
+		array( 'with-panel' => 'With Panel on dark' ),
+		array( 'split-with-image' => 'Split with Image' )
 	)
-)
-->conditional( 'cta_style', '==', 'simple' )
-->addColorPicker( 'background_color' )
-->conditional( 'checkbox_field', '==', 'is Background on brand?' )
-    ->addImage(
-		'featured_image',
+
+	->addCheckbox(
+		'checkbox_field',
 		array(
-			'preview_size'  => 'medium',
-			'return_format' => 'id',
+			'label'         => 'On Brand',
+			'choices'       => array( 'is Background on brand?' ),
+			'default_value' => array( 0 ),
+			'return_format' => 'value',
 		)
 	)
-	->addText( 'subtitle' )
-	->addText( 'title' )
-	->addTextarea( 'description' )
-    ->addRepeater(
-		'list',
-		array(
-			'max'  => '8',
+
+	->conditional( 'cta_style', '==', 'simple' )
+		->addColorPicker( 'background_color' )
+			->conditional( 'checkbox_field', '==', 'is Background on brand?' )
+
+		->addImage(
+			'featured_image',
+			array(
+				'preview_size'  => 'medium',
+				'return_format' => 'id',
+			)
 		)
-	)
-	->conditional( 'cta_style', '==', 'two-columns' )
-		->addText( 'list_title' )
-	->endRepeater()
+		->addText( 'subtitle' )
+		->addText( 'title' )
+		->addTextarea( 'description' )
+			->addRepeater(
+				'list',
+				array(
+					'max'  => '8',
+				)
+			)
+			->conditional( 'cta_style', '==', 'two-columns' )
+				->addText( 'list_title' )
+		->endRepeater()
+
 	->addText( 'link_text' )
 	->addPageLink( 'page_link' )
 	->addText( 'link_text_2' )
-    ->conditional( 'cta_style', '==', 'simple' )
+		->conditional( 'cta_style', '==', 'simple' )
+
 	->addPageLink( 'page_link_2' )
-    ->conditional( 'cta_style', '==', 'simple' )
+		->conditional( 'cta_style', '==', 'simple' )
 
-
-	->setLocation( 'block', '==', 'acf/cta' );
+->setLocation( 'block', '==', 'acf/cta' );
 
 return $cta;
