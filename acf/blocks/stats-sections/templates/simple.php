5f5da86eb2bca8ac1d2324043b00bf5937d6d720
Jun Mar M. Manuel <122262818+Pongs22@users.noreply.github.com>
2024-01-15 15:03:43 +0800

    Change placeholder text color

--- acf/blocks/stats-sections/templates/simple.php
+++ acf/blocks/stats-sections/templates/simple.php
@@ -41,16 +41,16 @@ if ( 'is-style-dark' === $style ) {
 		<?php else : ?>
 			<dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
 				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
-					<dt class="text-base leading-7 text-gray-600">Transactions every 24 hours</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">44 million</dd>
+					<dt class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Transactions every 24 hours</dt>
+					<dd class="order-first text-3xl font-semibold tracking-tight <?php echo esc_attr( $text_color_primary ); ?> sm:text-5xl">44 million</dd>
 				</div>
 				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
-					<dt class="text-base leading-7 text-gray-600">Assets under holding</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">$119 trillion</dd>
+					<dt class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Assets under holding</dt>
+					<dd class="order-first text-3xl font-semibold tracking-tight <?php echo esc_attr( $text_color_primary ); ?> sm:text-5xl">$119 trillion</dd>
 				</div>
 				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
-					<dt class="text-base leading-7 text-gray-600">New users annually</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">46,000</dd>
+					<dt class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">New users annually</dt>
+					<dd class="order-first text-3xl font-semibold tracking-tight <?php echo esc_attr( $text_color_primary ); ?> sm:text-5xl">46,000</dd>
 				</div>
 			</dl>
 		<?php endif; ?>
