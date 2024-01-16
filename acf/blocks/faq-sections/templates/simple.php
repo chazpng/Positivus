1c743624ec5caeb3e3838c6e06e4bfa7d0126f51
Jun Mar M. Manuel <122262818+Pongs22@users.noreply.github.com>
2024-01-16 12:04:15 +0800

    Added new Template for FAQ Section

--- acf/blocks/faq-sections/templates/simple.php
+++ acf/blocks/faq-sections/templates/simple.php
@@ -8,11 +8,13 @@
 ?>
 
 <?php
-$bg_color             = '';
-$text_color_primary   = 'text-gray-900';
-$text_color_secondary = 'text-gray-600';
-$description          = get_field( 'description' ) ?: 'Lorem ipsum dolor sit amet consect adipisicing possimus.';
-$main_title           = get_field( 'title' ) ?: 'Frequently asked questions';
+$bg_color                     = '';
+$text_color_primary           = 'text-gray-900';
+$text_color_secondary         = 'text-gray-600';
+$description                  = get_field( 'description' ) ?: 'Lorem ipsum dolor sit amet consect adipisicing possimus.';
+$main_title                   = get_field( 'title' ) ?: 'Frequently asked questions';
+$intro_container_two_column   = '';
+$intro_container_three_column = '';
 
 
 
@@ -24,19 +26,88 @@ if ( 'is-style-dark' === $style ) {
 	$stats_background_color = 'bg-white/5';
 	$button_style           = 'rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold <?php echo esc_attr($text_color_primary); ?> shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white';
 }
-?>
 
-<div class="<?php echo esc_attr( $bg_color ); ?>">
-	<div class="mx-auto max-w-7xl px-6 py-24 sm:pt-32 lg:px-8 lg:py-40">
-		<div class="lg:grid lg:grid-cols-12 lg:gap-8">
-			<div class="lg:col-span-5">
-				<h2 class="text-2xl font-bold leading-10 tracking-tight <?php echo esc_attr( $text_color_primary ); ?> mb-4"><?php echo esc_html( $main_title ); ?></h2>
+switch ( $align ) {
+	case 'center':
+		$intro_container_three_column = 'mx-auto max-w-2xl text-center';
+		$intro_container_two_column   = 'mx-auto max-w-2xl text-center';
+		break;
+
+	case 'right':
+		$intro_container_three_column = 'ml-auto max-w-2xl text-right';
+		$intro_container_two_column   = 'ml-auto max-w-2xl text-right';
+
+	default:
+		break;
+}
+?>
+<?php if ( 'offset' === get_field( 'simple_type' ) ) : ?>
+	<div class="<?php echo esc_attr( $bg_color ); ?>">
+		<div class="mx-auto max-w-7xl px-6 py-24 sm:pt-32 lg:px-8 lg:py-40">
+			<div class="lg:grid lg:grid-cols-12 lg:gap-8">
+				<div class="lg:col-span-5">
+					<h2 class="text-2xl font-bold leading-10 tracking-tight <?php echo esc_attr( $text_color_primary ); ?> mb-4"><?php echo esc_html( $main_title ); ?></h2>
+					<div><?php echo wp_kses_post( get_field( 'description' ) ); ?></div>
+				</div>
+				<div class="mt-10 lg:col-span-7 lg:mt-0">
+					<?php if ( have_rows( 'questions' ) ) : ?>
+						<dl class="space-y-10">
+							<?php
+							while ( have_rows( 'questions' ) ) :
+								the_row();
+								$question = get_sub_field( 'question' ) ?: 'Question';
+								$answer   = get_sub_field( 'answer' ) ?: 'Answer';
+								?>
+								<div>
+									<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>"><?php echo esc_html( $question ); ?></dt>
+									<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>"><?php echo esc_html( $answer ); ?></dd>
+								</div>
+							<?php endwhile; ?>
+						</dl>
+					<?php else : ?>
+						<dl class="space-y-10">
+							<div>
+								<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">How do you make holy water?</dt>
+								<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							</div>
+							<div>
+								<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What's the best thing about Switzerland?</dt>
+								<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							</div>
+							<div>
+								<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What do you call someone with no body and no nose?</dt>
+								<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							</div>
+							<div>
+								<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">Why do you never see elephants hiding in trees?</dt>
+								<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Because they're so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							</div>
+						</dl>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endif; ?>
+<?php 
+if ( 'two-column' === get_field( 'simple_type' ) || 'three-column' === get_field( 'simple_type' ) ) :
+	$intro_container = ( 'two-column' === get_field( 'simple_type' ) ) ? $intro_container_two_column : $intro_container_three_column;
+
+	$faq_container_two_column   = 'space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:gap-x-10';
+	$faq_container_three_column = 'space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:grid-cols-3 lg:gap-x-10';
+	$faq_container              = ( 'two-column' === get_field( 'simple_type' ) ) ? $faq_container_two_column : $faq_container_three_column;
+
+	?>
+	<div class="<?php echo esc_attr( $bg_color ); ?>">
+		<div class="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
+			<div class="<?php echo esc_attr( $intro_container ); ?>">
+				<h2 class="text-2xl font-bold leading-10 tracking-tight <?php echo esc_attr( $text_color_primary ); ?>"><?php echo esc_html( $main_title ); ?></h2>
 				<div><?php echo wp_kses_post( get_field( 'description' ) ); ?></div>
 			</div>
-			<div class="mt-10 lg:col-span-7 lg:mt-0">
+			<div class="mt-20">
 				<?php if ( have_rows( 'questions' ) ) : ?>
-					<dl class="space-y-10">
-						<?php 
+					<dl class="<?php echo esc_attr( $faq_container ); ?>">
+						<?php
 						while ( have_rows( 'questions' ) ) :
 							the_row();
 							$question = get_sub_field( 'question' ) ?: 'Question';
@@ -49,26 +120,34 @@ if ( 'is-style-dark' === $style ) {
 						<?php endwhile; ?>
 					</dl>
 				<?php else : ?>
-					<dl class="space-y-10">
+					<dl class="<?php echo esc_attr( $faq_container ); ?>">
 						<div>
-							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">How do you make holy water?</dt>
-							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What&#039;s the best thing about Switzerland?</dt>
+							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
 						</div>
 						<div>
-							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What's the best thing about Switzerland?</dt>
-							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What&#039;s the best thing about Switzerland?</dt>
+							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
 						</div>
 						<div>
-							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What do you call someone with no body and no nose?</dt>
-							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What&#039;s the best thing about Switzerland?</dt>
+							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
 						</div>
 						<div>
-							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">Why do you never see elephants hiding in trees?</dt>
-							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Because they're so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What&#039;s the best thing about Switzerland?</dt>
+							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+						</div>
+						<div>
+							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What&#039;s the best thing about Switzerland?</dt>
+							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
+						</div>
+						<div>
+							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?>">What&#039;s the best thing about Switzerland?</dt>
+							<dd class="mt-2 text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</dd>
 						</div>
 					</dl>
 				<?php endif; ?>
 			</div>
 		</div>
 	</div>
-</div>
+<?php endif; ?>
