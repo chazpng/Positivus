9431339ec4c5f203f10a36e55eeff8e8934f330d
Jun Mar M. Manuel <122262818+Pongs22@users.noreply.github.com>
2024-01-16 14:43:32 +0800

    Added new template for FAQ section

--- acf/blocks/faq-sections/templates/simple.php
+++ acf/blocks/faq-sections/templates/simple.php
@@ -11,6 +11,7 @@
 $bg_color                     = '';
 $text_color_primary           = 'text-gray-900';
 $text_color_secondary         = 'text-gray-600';
+$divider_color                = 'divide-gray-900/10';
 $description                  = get_field( 'description' ) ?: 'Lorem ipsum dolor sit amet consect adipisicing possimus.';
 $main_title                   = get_field( 'title' ) ?: 'Frequently asked questions';
 $intro_container_two_column   = 'max-w-2xl';
@@ -24,6 +25,7 @@ if ( 'is-style-dark' === $style ) {
 	$text_color_label       = 'text-indigo-400';
 	$bg_color               = 'bg-gray-900';
 	$stats_background_color = 'bg-white/5';
+	$divider_color          = 'divide-white/20';
 	$button_style           = 'rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold <?php echo esc_attr($text_color_primary); ?> shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white';
 }
 
@@ -89,7 +91,7 @@ switch ( $align ) {
 		</div>
 	</div>
 <?php endif; ?>
-<?php 
+<?php
 if ( 'two-column' === get_field( 'simple_type' ) || 'three-column' === get_field( 'simple_type' ) ) :
 	$intro_container = ( 'two-column' === get_field( 'simple_type' ) ) ? $intro_container_two_column : $intro_container_three_column;
 
@@ -148,6 +150,69 @@ if ( 'two-column' === get_field( 'simple_type' ) || 'three-column' === get_field
 					</dl>
 				<?php endif; ?>
 			</div>
+		</div>
+	</div>
+<?php endif; ?>
+<?php if ( 'side' === get_field( 'simple_type' ) ) : ?>
+	<div class="<?php echo esc_attr( $bg_color ); ?>">
+		<div class="mx-auto max-w-7xl divide-y <?php echo esc_attr( $divider_color ); ?> px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+			<h2 class="text-2xl font-bold leading-10 tracking-tight <?php echo esc_attr( $text_color_primary ); ?>"><?php echo esc_html( $main_title ); ?></h2>
+			<?php if ( have_rows( 'questions' ) ) : ?>
+				<dl class="mt-10 space-y-8 divide-y <?php echo esc_attr( $divider_color ); ?>">
+					<?php 
+					while ( have_rows( 'questions' ) ) :
+						the_row();
+						$question = get_sub_field( 'question' ) ?: 'Question';
+						$answer   = get_sub_field( 'answer' ) ?: 'Answer';
+						?>
+						<div class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
+							<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?> lg:col-span-5"><?php echo esc_html( $question ); ?></dt>
+							<dd class="mt-4 lg:col-span-7 lg:mt-0">
+								<p class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>"><?php echo esc_html( $answer ); ?></p>
+							</dd>
+						</div>
+					<?php endwhile; ?>
+				</dl>
+			<?php else : ?>
+				<dl class="mt-10 space-y-8 divide-y <?php echo esc_attr( $divider_color ); ?>">
+					<div class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
+						<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?> lg:col-span-5">What&#039;s the best thing about Switzerland?</dt>
+						<dd class="mt-4 lg:col-span-7 lg:mt-0">
+							<p class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+						</dd>
+					</div>
+					<div class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
+						<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?> lg:col-span-5">Why do you never see elephants hiding in trees?</dt>
+						<dd class="mt-4 lg:col-span-7 lg:mt-0">
+							<p class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Because they&#039;re so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+						</dd>
+					</div>
+					<div class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
+						<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?> lg:col-span-5">Why can&#039;t you hear a pterodactyl go to the bathroom?</dt>
+						<dd class="mt-4 lg:col-span-7 lg:mt-0">
+							<p class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+						</dd>
+					</div>
+					<div class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
+						<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?> lg:col-span-5">How do you make holy water?</dt>
+						<dd class="mt-4 lg:col-span-7 lg:mt-0">
+							<p class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+						</dd>
+					</div>
+					<div class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
+						<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?> lg:col-span-5">What do you call someone with no body and no nose?</dt>
+						<dd class="mt-4 lg:col-span-7 lg:mt-0">
+							<p class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+						</dd>
+					</div>
+					<div class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8">
+						<dt class="text-base font-semibold leading-7 <?php echo esc_attr( $text_color_primary ); ?> lg:col-span-5">Why did the invisible man turn down the job offer?</dt>
+						<dd class="mt-4 lg:col-span-7 lg:mt-0">
+							<p class="text-base leading-7 <?php echo esc_attr( $text_color_secondary ); ?>">He couldn&#039;t see himself doing it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.</p>
+						</dd>
+					</div>
+				</dl>
+			<?php endif; ?>
 		</div>
 	</div>
 <?php endif; ?>
