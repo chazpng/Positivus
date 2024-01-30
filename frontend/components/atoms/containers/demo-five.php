35c8fbea83cbd2a36b625b206de7bbcc89914ee5
jaye <jaye@greydientlab.com>
2024-01-30 19:12:57 +0800

    added comments

--- frontend/components/atoms/containers/demo-five.php
+++ frontend/components/atoms/containers/demo-five.php
@@ -14,14 +14,16 @@
 	<div class="bg-white">
 		<div class="mx-auto max-w-3xl">
 			<div class="relative h-64 overflow-hidden border border-dashed border-gray-400 opacity-75">
-				<svg class="absolute inset-0 h-full w-full stroke-gray-900/10" fill="none">
-					<defs>
-						<pattern id="pattern-c9405892-1693-467c-891e-c01d12d79e85" x="0" y="0" width="10" height="10" patternUnits="userSpaceOnUse">
-							<path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3"></path>
-						</pattern>
-					</defs>
-					<rect stroke="none" fill="url(#pattern-c9405892-1693-467c-891e-c01d12d79e85)" width="100%" height="100%"></rect>
-				</svg>
+				<!-- Replace with real content start -->
+					<svg class="absolute inset-0 h-full w-full stroke-gray-900/10" fill="none">
+						<defs>
+							<pattern id="pattern-c9405892-1693-467c-891e-c01d12d79e85" x="0" y="0" width="10" height="10" patternUnits="userSpaceOnUse">
+								<path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3"></path>
+							</pattern>
+						</defs>
+						<rect stroke="none" fill="url(#pattern-c9405892-1693-467c-891e-c01d12d79e85)" width="100%" height="100%"></rect>
+					</svg>
+				<!-- Replace with real content end -->
 			</div>
 		</div>
 	</div>
