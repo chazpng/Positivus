b7c621339961e87c99a415e3ab5e7b42ade7f9b3
jaye <jaye@greydientlab.com>
2024-02-08 18:07:36 +0800

    added stacked-flyout-menu styling and functionality

--- frontend/resources/js/main.js
+++ frontend/resources/js/main.js
@@ -1,7 +1,7 @@
 /* global jQuery */
 
 jQuery( function( $ ) {
-	$( '.menu-item-has-children' ).click( function( e ) {
+	$( '#stacked-flyout-menu .menu-item-has-children' ).click( function( e ) {
 		e.preventDefault();
 		$( '.sub-menu', this ).toggleClass( 'show-sub-menu' );
 	} );
@@ -11,5 +11,18 @@ jQuery( function( $ ) {
 		if ( ! container.is( e.target ) && container.has( e.target ).length === 0 ) {
 			container.removeClass( 'show-sub-menu' );
 		}
+	} );
+
+	$( '#mobile-stacked-flyout-menu .menu-item-has-children' ).click( function( e ) {
+		e.preventDefault();
+		$( '.sub-menu', this ).toggle();
+	} );
+
+	$( '.stacked-flyout-menu-header .drawer-toggle' ).click( function() {
+		$( '.stacked-flyout-menu-header .drawer' ).toggle();
+	} );
+
+	$( window ).resize( function() {
+		$( '.stacked-flyout-menu-header .drawer' ).hide();
 	} );
 } );
