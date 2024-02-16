2d6cee4af069872f5dcad9d72abf7c84426ce2f5
Pongs22 <junmar@greydientlab.com>
2024-02-16 19:58:08 +0800

    Initial commit on mac

--- acf/blocks/pricing/pricing.php
+++ acf/blocks/pricing/pricing.php
@@ -10,12 +10,6 @@ $pricing_style = get_field( 'pricing_style' );
 $align         = $block['align'];
 ?>
 <div class="gl-b-pricing-section">
-<?php if ( 'stepped' === $pricing_style ) : ?>
-		<?php require gl_get_custom_block_template( 'pricing', 'stepped.php' ); ?>
-	<?php endif; ?>
-	<?php if ( 'with-image' === $pricing_style ) : ?>
-		<?php require gl_get_custom_block_template( 'pricing', 'with-image.php' ); ?>
-	<?php endif; ?>
 	<?php if ( 'simple' === $pricing_style ) : ?>
 		<?php require gl_get_custom_block_template( 'pricing', 'simple.php' ); ?>
 	<?php endif; ?>
