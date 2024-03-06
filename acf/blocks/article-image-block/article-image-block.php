46a67f880a1493d4d3081388ab61134e7b5e7504
Jun Mar M. Manuel <122262818+Pongs22@users.noreply.github.com>
2024-03-06 17:29:36 +0800

    Fixed article image styling

--- acf/blocks/article-image-block/article-image-block.php
+++ acf/blocks/article-image-block/article-image-block.php
@@ -10,7 +10,7 @@
 	<?php echo wp_get_attachment_image( get_field( 'image' ), 'full', '', array( 'class' => 'h-full w-full object-contain rounded-xl' ) ); ?>
 	<?php if ( get_field( 'description' ) ) : ?>
 		<div class="description">
-		<p class="text-gray-600 text-sm mt-4 !mb-0"><?php echo esc_html( get_field( 'description' ) ); ?> 
+		<p class="text-gray-600 !text-sm mt-4 !mb-0"><?php echo esc_html( get_field( 'description' ) ); ?> 
 		<?php 
 		if ( get_field( 'credentials_name' ) && get_field( 'credentials_url' ) ) :
 			?>
