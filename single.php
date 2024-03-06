9c1ca4933ece7321a4deca70380efe898139cc36
Jun Mar M. Manuel <122262818+Pongs22@users.noreply.github.com>
2024-03-06 17:16:24 +0800

    Fixed lint error, added border and fixed border radius

--- single.php
+++ single.php
@@ -23,27 +23,29 @@ get_header();
 
 			<?php foreach ( $blocks as $block ) : ?>
 				<?php if ( 2 === $count ) : ?>
-					<div class="container mx-auto py-16">
-						<div class="grid lg:grid-cols-12 mx-auto lg:gap-x-8 ">
-							<div class="hidden lg:block lg:col-span-3  relative">
-								<div class="table-wrapper sticky top-10 px-4 py-6 bg-gray-100/50 rounded-md text-lg">
-								<p class="label text-lg font-semibold">Table of contents</p>
-								<ul class="text-gray-600 gap-y-2 flex flex-col text-base mt-6">
-									<li><a href="#">Animation easing</a></li>
-									<li><a href="#">Colour guidelines</a></li>
-									<li><a href="#">Fluid typography</a></li>
-									<li><a href="#">Other resources</a></li>
-									<li><a href="#">Other resources</a></li>
-									<li><a href="#">Conclusion</a></li>
-								</ul>
+					<div class="border-t">
+						<div class="container mx-auto py-16 ">
+							<div class="grid lg:grid-cols-12 mx-auto lg:gap-x-8 ">
+								<div class="hidden lg:block lg:col-span-3  relative">
+									<div class="table-wrapper sticky top-10 px-4 py-6 bg-gray-100/50 rounded-xl text-lg">
+										<p class="label text-lg font-semibold">Table of contents</p>
+										<ul class="text-gray-600 gap-y-2 flex flex-col text-base mt-6">
+											<li><a href="#">Animation easing</a></li>
+											<li><a href="#">Colour guidelines</a></li>
+											<li><a href="#">Fluid typography</a></li>
+											<li><a href="#">Other resources</a></li>
+											<li><a href="#">Other resources</a></li>
+											<li><a href="#">Conclusion</a></li>
+										</ul>
+									</div>
 								</div>
-							</div>
-							<div class="lg:col-span-9 post-content">
-							<?php endif ?>
+								<div class="lg:col-span-9 post-content">
+								<?php endif ?>
 
-							<?php echo wp_kses_post( render_block( $block ) ); ?>
+								<?php echo wp_kses_post( render_block( $block ) ); ?>
 
-							<?php if ( $count >= $block_length ) : ?>
+								<?php if ( $count >= $block_length ) : ?>
+								</div>
 							</div>
 						</div>
 					</div>
