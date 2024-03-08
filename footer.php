08441d45d1d5aba13f81b817a3f5e75f91b96fad
Jun Mar M. Manuel <122262818+Pongs22@users.noreply.github.com>
2024-03-08 11:14:27 +0800

    Updated Footer

--- footer.php
+++ footer.php
@@ -11,24 +11,15 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<!-- <div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'greydientlab' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'greydientlab' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'greydientlab' ), 'greydientlab', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div>.site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer py-4 bg-gray-100 text-gray-600 text-sm leading-6 ">
+	<div class="container mx-auto ">
+		Copyright (C) Greydient Lab 2024
+	</div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
