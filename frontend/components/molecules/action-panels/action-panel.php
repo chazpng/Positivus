09e017421ab26fd47cb0a50a8ab50bf456d25a7d
Jun Mar M. Manuel <122262818+Pongs22@users.noreply.github.com>
2024-03-15 18:35:41 +0800

    Fixed action panel styles

--- frontend/components/molecules/action-panels/action-panel.php
+++ frontend/components/molecules/action-panels/action-panel.php
@@ -9,18 +9,18 @@
  * @package greydientlab
  */
 
-$gl_text_title                   = $args['gl_text_title'] ?? false;
-$gl_text_description             = $args['gl_text_description'] ?? false;
-$gl_text_container_class         = $args['gl_text_container_class'] ?? '';
-$gl_text_type                    = $args['gl_text_type'] ?? 'simple';
-$gl_text_title_class             = $args['gl_text_title_class'] ?? 'text-lg font-bold';
-$gl_text_description_class       = $args['gl_text_description_class'] ?? 'mt-1';
-$gl_btn_type                     = $args['gl_btn_type'] ?? false;
-$gl_btn_size                     = $args['gl_btn_size'] ?? false;
-$gl_btn_text                     = $args['gl_btn_text'] ?? '';
-$gl_action_panel_outer_container = $args['gl_action_panel_container'] ?? '';
-$gl_action_panel_container       = $args['gl_action_outer_container'] ?? '';
-$gl_action_panel_button          = $args['gl_action_panel_button'] ?? '';
+$gl_text_title             = $args['gl_text_title'] ?? false;
+$gl_text_description       = $args['gl_text_description'] ?? false;
+$gl_text_container_class   = $args['gl_text_container_class'] ?? '';
+$gl_text_type              = $args['gl_text_type'] ?? 'simple';
+$gl_text_title_class       = $args['gl_text_title_class'] ?? 'text-lg font-bold';
+$gl_text_description_class = $args['gl_text_description_class'] ?? 'mt-1';
+$gl_btn_type               = $args['gl_btn_type'] ?? false;
+$gl_btn_size               = $args['gl_btn_size'] ?? false;
+$gl_btn_text               = $args['gl_btn_text'] ?? '';
+$gl_action_panel_container = $args['gl_action_panel_container'] ?? '';
+$gl_action_outer_container = $args['gl_action_outer_container'] ?? '';
+$gl_action_panel_button    = $args['gl_action_panel_button'] ?? '';
 ?>
 <div class="<?php echo esc_attr( $gl_action_panel_outer_container ); ?>">
 	<div class="<?php echo esc_attr( $gl_action_panel_container ); ?>">
@@ -38,10 +38,10 @@ $gl_action_panel_button          = $args['gl_action_panel_button'] ?? '';
 				'gl_description_class' => $gl_text_description_class,
 				'gl_class'             => $gl_text_container_class,
 			]
-		);
+		); 
 		?>
 		<div class="<?php echo esc_attr( $gl_action_panel_button ); ?>">
-			<?php
+			<?php 
 			Load::atom(
 				'buttons/button',
 				[
@@ -49,8 +49,11 @@ $gl_action_panel_button          = $args['gl_action_panel_button'] ?? '';
 					'gl_btn_size' => $gl_btn_size,
 					'gl_btn_text' => $gl_btn_text,
 				]
-			);
+			); 
 			?>
 		</div>
 	</div>
 </div>
+
+
+
